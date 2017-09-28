884b953185870bd247ba6646ee12b1f69cddd529
emmadema <emma.demango@gmail.com>
2017-09-27 20:36:30 -0600

    its working

--- Game/my2.js
+++ Game/my2.js
@@ -18,55 +18,63 @@ $(document).ready(function(){
 //use two boards on one screen split with sperate scores then comapre the seprate scores
 
 
-		//var points = 0;
-		//var score = $();
+	var points = 0;
+	var score = $();
 
-		/*var topLeft = $('#topLeft');
-		var topMiddle = $('#topMiddle');
-		var topRight = $('#topRight');
-		var bottomLeft = $('#bottomLeft');
-		var bottomMiddle = $('#bottomMiddle');
-		var bottomRight = $('#bottomRight');
-		
-		var cards = [topLeft, topMiddle, topRight, bottomLeft, bottomMiddle, bottomRight];
+	var clicks = 0;
+	var classNames = [];
 
-		//when a card is clicked set the attribute of one of those */
+	var flipped1 = $('.reptar').length; 
+	var flipped2 = $('.tommy').length;
+	var flipped3 = $('.angelica').length;//create a variable for what is cliked (this) and pull its class
+	var flipped = [flipped1, flipped2, flipped3];
 
+		//var flipped = $('')
 
 	//flip the cards when a cell is clicked
 	$('.cells').click(function(){ //when a cell is clicked run this function
 		$(this).find('.cardFront').show();//find only the card front of the element that is clicked 
-		$(this).find('.cardBack').hide();//find only the card back of the elemnt that is clicked and hide it	
-		var flipped1 = $('.reptar').length; 
-		var flipped2 = $('.tommy').length;
-		var flipped3 = $('.angelica').length;//create a variable for what is cliked (this) and pull its class
-		var flipped = [flipped1, flipped2, flipped3];	
-			if (flipped === 2) { //if the # of flipped cards = 2 then move on
-				console.log('cards flipped'); 	
-			} 
+		$(this).find('.cardBack').hide();//find only the card back of the elemnt that is clicked and hide it		
+		classNames.push($(this).find('.cardFront'));
+		console.log(classNames);
+		if (clicks === 0) {
+			clicks++;
+			//dont compare
 
+		} else if (clicks === 1) {
+			clicks=0;
+			//compare!
+			if (classNames[0].attr('class') === classNames[1].attr('class')){
+				//dont allow another click 
+				$(this).unbind("click");
+				setTimeout(alert, 500, "match");
+			} else {
+				$(this).find('.cardBack').show();
+				$(this).find('.cardFront').hide();
+								//
+				// show();
 
-	});
+				classNames[0].hide();
+				classNames[0].parent().find('.cardBack').show();
 
-		//function matchingCards (){
+			}
+		classNames =[];
+		}
 		
 
-		//matchingCards();
-			
-
-			//var firstCard = flipped.first(); //makes a varibale for the first flipped card
-			//var secondCard = flipped.last(); //makes a varibale for the second flipped card
-			//if(firstCard.attr('class') === secondCard.attr('class')) { //if the first cards class = second cards class
-				//points ++;//add 1 point to the totoal points
-				//console.log("You found a match!"); //alert the user that they have a match
-				//firstCard.remove();//remove so the function will run again for the next match
-				//secondCard.remove(); //remove so the function will run again for the next match
-			//}
-
-
-
-
-	
+			// if (flipped.length === 2) { //if the # of flipped cards = 2 then move on
+			// 	console.log('cards flipped');	
+			// } 
+	});
+			/*var firstCard = flipped.first(); //makes a varibale for the first flipped card
+			var secondCard = flipped.last(); //makes a varibale for the second flipped card
+			if(firstCard.attr('class') === secondCard.attr('class')) { //if the first cards class = second cards class
+				points ++;//add 1 point to the totoal points
+				console.log("You found a match!"); //alert the user that they have a match
+				firstCard.remove();//remove so the function will run again for the next match
+				secondCard.remove(); //remove so the function will run again for the next match
+			}*/
+		
 	//if number of cards =2 see if thier ids match
 	// if they do match give a point
 	//if two cards are slected even if they dont match flip them back over
@@ -75,8 +83,6 @@ $(document).ready(function(){
 	//assigning cards randomly at the start of the game
 	//give each card a class and write out random sequences
 	//use a function to sort an array of cards randomly into each cell
-
-	
 
 	//reset button 
 	$('.refresh').click(function(){//when the refresh button is clicked run this function
