3a0ea226bf2a1de6e434e8d55e7871db63522d52
emmadema <emma.demango@gmail.com>
2017-09-29 11:38:36 -0600

    all comments added

--- gameTwoPlayer/my3.js
+++ gameTwoPlayer/my3.js
@@ -2,10 +2,10 @@
 $(document).ready(function(){
 	console.log('query ready');
 
-	var points1 = 0;//tracks how many matched each player got
-	var points2 = 0;
-	var score1 = 0;//keeps score for each player (how many games they won)
-	var score2 = 0;
+	var points1 = 0;//tracks how many matches player one got
+	var points2 = 0;//track how many matches player 2 got
+	var score1 = 0;//keeps score for player one (how many games they won)
+	var score2 = 0;//keeps  track of score for player two (games won)
 
 	var clicks = 0;//counts the number of clicks each player has made
 	var classNames = []; //calls names array feeds in the class names for the click function and then moves them out
@@ -26,39 +26,36 @@ $(document).ready(function(){
 				clicks=0;//move the clicks back to 0 and move on to compare the cards
 				//compare
 				if (classNames[0].attr('class') === classNames[1].attr('class')) {//if the classNames match up in the array for both 1 and 2 execute code
-					//$(this).unbind('click');//dont allow another click 
 					classNames = []; //clear the array after a match
-					//setTimeout(alert, 250, "Player 1 got a match!"); //send alert that you have a match, but dont run it until the card is flipped over
 					points1++;//ad one point each time
-						$(".pointsOne div").text("");
+						$(".pointsOne div").text("");//sets points on to empty string
 						$(".pointsOne div").append(points1); //display the points
-						console.log(points1);
+						//console.log(points1);
 						playerTurn = "player_2";//set the player turn to player to move on
 						setTimeout(alert, 500, 'Player 2 Turn'); //annouce that it is player twos turn only after 1.5 seconds
 					if (points1 === 3){//if points are three then execute code
 						setTimeout(alert, 1000, "Player 1 Wins!"); //send alert that player one wins only after 1.5 seconds
 						score1++;//add one point to player ones score
-						$(".scoreOne div").text("");
+						$(".scoreOne div").text("");//sets score one to empty string
 						$(".scoreOne div").append(score1);//append player ones score to the game board
 					}
 
 				} 
 				else { //if there is no match then show the card back and hide the card front
-						setTimeout( () => {
+						setTimeout( () => {//this arraw function sets a timout and allows the previous this to be passed into this function
 						playerTurn = "player_2";//set players turn to player two to move on	
 						$(this).find('.cardBack').show();//when I remove this the card clicked on secong dissaperes
 						$(this).find('.cardFront').hide();//hide the front of the card
 						classNames[0].hide();//when I remove this the first card dissappersand the second card appers under the card Back
 						classNames[0].parent().find('.cardBack').show();//when I remove this the first card clicked disspears and the second card never flips
-						classNames = [];
+						classNames = [];//sets class names to ewmpty array
 
-					}, 250);
-					setTimeout(alert, 500, "No match, Player Two's Turn");
+					}, 250);//delays this block of codse by 1/4 second
+					setTimeout(alert, 500, "No match, Player Two's Turn");//alerts player not match and it is next players turn
 				}
 			}
 		} 
 		else {//if it is player twos turn execute the below code
-			//alert("Player Two's Turn");
 			$(this).find('.cardFront').show();//find only the card front of the element that is clicked 
 			$(this).find('.cardBack').hide();//find only the card back of the elemnt that is clicked and hide it		
 			classNames.push($(this).find('.cardFront'));//push the class name of card front into the array classNames
@@ -71,13 +68,11 @@ $(document).ready(function(){
 				clicks=0; // move the clicks back to 0 and move on to compare the cards
 				//compare
 				if (classNames[0].attr('class') === classNames[1].attr('class')){//if the classNames match up in the array for both 1 and 2 execute code
-					//$(this).unbind('click');//dont allow another click 
 					classNames = []; //clear the array after a match
-					//setTimeout(alert, 250, "Player 2 got a match!"); //send alert that you have a match, but dont run it until the card is flipped over
 					points2++;//ad one point each time
-					$(".pointsTwo div").text("");
+					$(".pointsTwo div").text("");//sets points 2 to an empty string
 					$(".pointsTwo div").append(points2); //display the points
-					console.log(points2);
+					//console.log(points2);
 					playerTurn = "player_1";//set the turn back to player one
 					setTimeout(alert, 500, "Player One's Turn"); //alert player 1 turn after 1.5 seconds
 					if (points2 === 3){//if points are three then execute code
@@ -90,17 +85,18 @@ $(document).ready(function(){
 				}
 				else { //if there is no match then show the card back and hide the card front
 					
-					setTimeout( () => {
-						playerTurn = "player_1";
+					setTimeout( () => { //this keep the this from the main function and passes into this timeout fucntion
+						playerTurn = "player_1"; //changes the player turn back to plauyer one
 						$(this).find('.cardBack').show();//when I remove this the card clicked on secong dissaperes
 						$(this).find('.cardFront').hide();//hide the front of the card
 						classNames[0].hide();//when I remove this the first card dissappersand the second card appers under the card Back
 						classNames[0].parent().find('.cardBack').show();//when I remove this the first card clicked disspears and the second card never flips
-						classNames = [];
-						alert("No match, Player One's Turn");
+						classNames = [];//sets classNames back to an empty array
+						alert("No match, Player One's Turn"); //alerts that the palyer did not get a match and it is the next player turn
 
-					}, 250);
-			}	}
+					}, 250); //delays this by a 1/4 second
+				}
+			}	
 		}
 	});
 	//reset button 
@@ -108,12 +104,12 @@ $(document).ready(function(){
  		$('.cardFront').hide();//for every card hide the card front with the character
  		$('.cardBack').show();//for every card show the card back with the Rugrats logo
  		classNames=[]; //sets class names back to 0
- 		points1=0; //sets points back to 0
- 		$(".pointsOne div").text("");
-		$(".pointsOne div").append(points1); 
-		points2=0;
-		$(".pointsTwo div").text("");
-		$(".pointsTwo div").append(points2);
+ 		points1=0; //sets points 1 back to 0
+ 		$(".pointsOne div").text("");//sets the points one div back to an empty string
+		$(".pointsOne div").append(points1); //appends the empty string
+		points2=0;//sets point 2 to 0
+		$(".pointsTwo div").text(""); //sets the points two div back to an empty string
+		$(".pointsTwo div").append(points2);//appends the empty string
  		clicks=0; //sets clicks back to 0
   		playerTurn= "player_1";//set the player turn back to player one
   		alert("Player One's turn");//alert player one that it is their turn
